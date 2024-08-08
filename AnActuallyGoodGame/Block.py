eac35722dccda3531bfdfd7ad2b23c35936a0699
Not-MatanEilat <57912969+Not-MatanEilat@users.noreply.github.com>
2024-08-08 22:49:39 +0300

    Updated the name of the project as well as the way screens are started

--- AnActuallyGoodGame/Block.py
+++
@@ -1,8 +0,0 @@
-from Sprite import Sprite
-
-class Block(Sprite):
-    def __init__(self, game_scroll, x, y, width, height):
-        super().__init__(game_scroll, x, y, width, height)
-
-    def update(self, keyboard):
-        pass
