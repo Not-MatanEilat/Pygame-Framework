9a425275a24737165c199606e51d0cfdcc4f3507
Not-MatanEilat <57912969+Not-MatanEilat@users.noreply.github.com>
2024-08-09 12:58:57 +0300

    Delete PygameFramework directory

--- PygameFramework/Block.py
+++
@@ -1,8 +0,0 @@
-from Sprite import Sprite
-
-class Block(Sprite):
-    def __init__(self, camera, x, y, width, height):
-        super().__init__(camera, x, y, width, height)
-
-    def update(self, keyboard):
-        pass
